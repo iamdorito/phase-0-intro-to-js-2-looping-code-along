3be5c41b948efaa932471f12563a1e54562afb07
Dorothy <dorothychauu@gmail.com>
2022-12-16 09:30:01 -0500

    commit tree 8ea9eb462ca01686d83972f3de3406e2b30a6f6f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+let names =  ["Gabriella", "Brendan", "Luana", "Seb"];
+
+// function writeCards(names,e) {
+//     let newNames = [];
+//     for(let i = 0; i < names.length; i++) {
+//         newNames.push(`Thank you, ${names[i]}, for the wonderful ${e} gift!`);
+//     }
+//     return newNames;
+// }
+// writeCards(names);
+const writeCards = (names,e) => {
+    let newNames = []
+    for(let i = 0; i < names.length; i++) {
+    newNames.push(`Thank you, ${names[i]}, for the wonderful ${e} gift!`);
+}
+    return newNames;
+};
+
+function countDown(num) {
+    for(let i = num; i >= 0; i--) {
+        console.log(i);
+}};
+
+// const countDown = (num) => {
+//     for (let i = num; i >= 0; i--) {
+//         console.log(i);
+// }}
